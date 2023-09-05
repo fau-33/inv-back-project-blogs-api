5266c153994be142308181a78aaa3381268080dd
Flávio Félix <fau_felix30@hotmail.com>
2023-09-05 15:57:33 -0300

    Reenviando o REQ 10

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,26 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
     const BlogPost = sequelize.define('BlogPost', {
-        id: {
-            primaryKey: true,
-            autoIncrement: true,
-            type: DataTypes.INTEGER,
-        },
-        title: DataTypes.STRING,
-        content: DataTypes.STRING,
-        userId: DataTypes.INTEGER,
-        published: DataTypes.DATE,
-        updated: DataTypes.DATE,
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
     },
     {
-        underscored: true,
-        timestamps: false,
-        tableName: 'blog_posts',
+      underscored: true,
+      timestamps: false,
+      tableName: 'blog_posts',
     });
-
+  
     BlogPost.associate = (models) => {
-        BlogPost.belongsTo(models.User, {
-            foreignKey: 'userId',
-            as: 'user',
-        });
+      BlogPost.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user',
+      });
     };
+  
+    return BlogPost;
 };
--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
-          primaryKey: true,
-          autoIncrement: true,
-          type: DataTypes.INTEGER,
-        },
-        displayName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        image: DataTypes.STRING,
-      },
-      {
-        underscored: true,
-        timestamps: false,
-        tableName: 'users',
-      });
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'users',
+  });
 
-      User.associate = (models) => {
-        User.hasMany(models.BlogPost, {
-          foreignKey: 'userId',
-          as: 'BlogPost',
-        });
-      };
-    
-      return User;
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'blogPost',
+    });
+  };
+
+  return User;
 };
