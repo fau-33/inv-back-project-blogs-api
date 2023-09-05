a92874f92ef6f13de2d27d1bfa3df74dd08065cf
Flávio Félix <fau_felix30@hotmail.com>
2023-09-04 21:00:17 -0300

    Feito REQ 04

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // ...
 
-const { loginRoute } = require('./routes');
+const { loginRoute, userRoute } = require('./routes');
 
 const app = express();
 
@@ -15,6 +15,7 @@ app.use(express.json());
 
 // ...
 app.use('/login', loginRoute);
+app.use('/user', userRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
