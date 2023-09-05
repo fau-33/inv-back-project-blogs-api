e89c58d459d628f2698e75082db79282f14c03cd
Flávio Félix <fau_felix30@hotmail.com>
2023-09-05 13:48:58 -0300

    Feito o REQ 05

--- src/middleware/JWTValidation.js
+++ src/middleware/JWTValidation.js
@@ -2,21 +2,23 @@ const { getPayload } = require('../utils/handleToken');
 
 const jwtValidation = (req, res, next) => {
     try {
-        const { authorization } = req.body;
-
-        if (!authorization) {
-            return res.status(401).json({
-                message: 'Token not found',
-              });
-        }
-
-        const payload = getPayload(authorization);
-        req.payload = payload;
-
-        next();
+      const { authorization } = req.headers;
+  
+      if (!authorization) {
+        return res.status(401).json({
+          message: 'Token not found',
+        });
+      }
+  
+      const extractedAuthorization = authorization.split(' ')[1];
+    
+      const payload = getPayload(extractedAuthorization);
+      req.payload = payload;
+  
+      next();
     } catch (_error) {
-        res.status(401).json({ message: 'Expired or invalid token' });
+      res.status(401).json({ message: 'Expired or invalid token' });
     }
-};
-
-module.exports = jwtValidation;
+  };
+  
+  module.exports = jwtValidation;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,7 +8,11 @@ const {
     passwordValidation,
 } = require('../middleware/userValidation');
 
+const tokenValidation = require('../middleware/JWTValidation');
+
 const route = express.Router();
+
+route.get('/', tokenValidation, UserController.getUsers);
 
 route.post(
     '/',
